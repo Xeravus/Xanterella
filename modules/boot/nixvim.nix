87832d4ca1cb33348590b8748a1188fc19d22b7c
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-05-13 20:59:34 +0200

    Auto-Rebuild: 2026-05-13 20:59:34

--- modules/boot/nixvim.nix
+++ modules/boot/nixvim.nix
@@ -110,6 +110,9 @@
               live-grep-args = {
                 enable = true;
               };
+              file-browser = {
+                enable = true;
+              };
             };
           };
         };
