83af9d80a623750a71d868a32fc2e2bc34e3885e
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-05-14 19:06:19 +0200

    Auto-Rebuild: 2026-05-14 19:06:19

--- modules/server/audiobookshelf.nix
+++ modules/server/audiobookshelf.nix
@@ -24,7 +24,6 @@
         host = "0.0.0.0";
         port = 13378;
         openFirewall = true;
-        dataDir = "/mnt/server-data/nix/audiobookshelf/metadata";
       };
     };
     networking = {
