35b7aca0f200d6fb4c6658e50c51f9a59935d73f
Xeravus <cato.jenisch@gmail.com>
2026-05-30 15:31:34 +0200

    Auto-Rebuild: 2026-05-30 15:31:34

--- modules/desktop/hyprland/soomfon.nix
+++ modules/desktop/hyprland/soomfon.nix
@@ -27,7 +27,7 @@
     services = {
       udev = {
         extraRules = ''
-          SUBSYSTEM=="usb", ATTR{idVendor}=="1500", ATTR{idProduct}=="3001", MODE="0666", GROUP="plugdev"
+          SUBSYSTEM=="usb", ATTR{idVendor}=="5376", ATTR{idProduct}=="12289", MODE="0666", GROUP="plugdev"
         '';
       };
     };
