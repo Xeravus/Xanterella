adf04ee7d9d2fa7806535036905a7cd22b8f7cc7
Xeravus <cato.jenisch@gmail.com>
2026-06-03 18:02:44 +0200

    Auto-Rebuild: 2026-06-03 18:02:43

--- modules/essentials/tailscale.nix
+++ modules/essentials/tailscale.nix
@@ -50,34 +50,21 @@ in {
         systemPackages = with pkgs-unstable; [
           tailscale
         ];
-      };
-      systemd = {
-        services = {
-          tailscale-autoconnect = {
-            description = "Automatische Verbindung für Remote Install";
-            after = [
-              "tailscaled.service"
-              "network-online.target"
-            ];
-            wants = [
-              "tailscaled.service"
-              "network-online.target"
-            ];
-            wantedBy = [
-              "multi-user.target"
-            ];
-            serviceConfig = {
-              Type = "oneshot";
-              RemainAfterExit = true;
-            };
-            script = ''
-              until ${pkgs.curl}/bin/curl -s https://login.tailscale.com > /dev/null; do
-              sleep 1
-              done
-
-              ${pkgs.tailscale}/bin/tailscale up --hostname installer --reset --auth-key "${secrets.tailscalekey}"
-            '';
+        etc = {
+          "tailscale.key" = {
+            text = secrets.tailscalekey;
+            mode = "0400";
           };
+        };
+      };
+      services = {
+        tailscale = {
+          enable = true;
+          authKeyFile = "/etc/tailscale.key";
+          extraUpFlags = [
+            "--hostname=installer"
+            "--reset"
+          ];
         };
       };
     })
